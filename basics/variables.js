a21a622ed5f1ee566122527be80ac051db32d793
rohann <roh4nn001@gmail.com>
2026-06-14 15:28:36 +0000

    var

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 101 //it cant change! once it made 
+let accountName = "rohan"
+var accountEmail = "rup@gmail.com"
+accountPassword = "145"
+let accountState 
+
+/*
+Prefer not to use var 
+becoz of issue in block scope and functional Scope
+*/
+console.table([accountId, accountName, accountEmail , accountPassword, accountState]);
+
