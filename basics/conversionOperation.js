bad2c0288d0553ab96b80f003c707559ffbd9d95
rohann <roh4nn001@gmail.com>
2026-06-15 04:30:05 +0000

    conversion and datatypes

---
+++ basics/conversionOperation.js
@@ -0,0 +1,33 @@
+/*
+let score = "50abcg"
+console.log(typeof score);
+console.log(typeof (score)); // method ()
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+*/
+
+// let isLoggedin = 0
+/*
+let isLoggedin = ""
+let booleanIsloggedin = Boolean(isLoggedin)
+console.log(booleanIsloggedin);
+*/
+
+/*
+let stringNumber = 88
+
+let NumberString = String(stringNumber)
+console.log(NumberString);
+console.log(typeof NumberString);
+
+*/
+
+let gameScore = 100
+
+++gameScore;
+console.log(gameScore);
+
+
---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+// data types
+
+
+/*
+
+1. Number => 2 to power 53 ( range )
+2. string => ""
+3. bigint
+4. boolean
+5. null => standalone value ( represent of empty value ) [ it is a type of  object in js ]
+6. undefine =
+7.object 
+8. symbol => it is widely used in REACT.js for identifying unique symbol 
+9. typeof = used to find the type of datatype
+
+
+
+*/
